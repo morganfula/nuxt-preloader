74b2293fecf5a539a3fcd33f5175fff1abd308b4
morganfula <morgan.fula@me.com>
2023-11-21 10:17:01 +0100

    structure done

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="viewport">
+		<slot />
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+	*,
+	*::after,
+	*::before {
+		box-sizing: border-box;
+		padding: 0;
+		margin: 0;
+
+		border: 1px solid red;
+	}
+
+	body {
+		background-color: rgb(58, 58, 58);
+		overflow: hidden;
+		-webkit-font-smoothing: antialiased;
+		-mos-osx-font-smoothing: grayscale;
+		color: white;
+	}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="gallery">
+		<div class="gallery__wrap" v-for="_ in 10">
+			<div class="gallery__item" v-for="img in images">
+				<img class="gallery__img" :src="img" alt="" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	const images = [
+		'1.jpg',
+		'2.jpg',
+		'3.jpg',
+		'4.jpg',
+		'5.jpg',
+		'6.jpg',
+		'7.jpg',
+		'8.jpg',
+		'9.jpg',
+		'10.jpg',
+		'11.jpg',
+		'12.jpg',
+	];
+</script>
+
+<style lang="scss" scoped>
+	.gallery__wrap {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+	}
+	.gallery__item {
+		height: 20vw;
+	}
+	.gallery__img {
+		vertical-align: top;
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		object-position: center;
+	}
+</style>
